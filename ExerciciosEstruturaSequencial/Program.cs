f7e3a275acac3a142380d44aa73b0e75383a555c
Jean Marques <jmichelmarques21@outlook.com>
2023-04-18 09:20:48 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosEstruturaSequencial {
+    public class Program {
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Entre dois números inteiros para soma: ");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
